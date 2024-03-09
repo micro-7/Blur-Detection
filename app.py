9b619e3b9c82efd97a2dadf32ceb96a1af1a42d4
mihir-tandon <mihirtandon25@gmail.com>
2024-03-09 15:46:23 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
 
-from flask import Flask, render_template, redirect, flash, request #use fapp
-from database import User, add_to_db, open_db
+from flask import Flask, render_template,redirect,request, flash, session
+from database import User, add_to_db, File, open_db
+import os
+from werkzeug.utils import secure_filename
+from common.file_utils import *
+
 
 app = Flask(__name__)
 app.secret_key = 'thisissupersecretkeyfornoone'
@@ -20,20 +24,42 @@ def login():
         #logic
     return render_template('login.html')
 
-@app.route('/signup', methods=['GET', 'POST']) # use froute
-def signup():
+@app.route('/register', methods=['GET', 'POST']) # use froute
+def register():
     if request.method == 'POST':
+        username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        print("Email =>", email)
-        print("Password =>", password)
-        #logic
-    return render_template('signup.html')
+        cpassword = request.form.get('cpassword')
+        print(username, email, password, cpassword)
+        # logic
+        if len(username) == 0 or len(email) == 0 or len(password) == 0 or len(cpassword) == 0:
+            flash("All fields are required", 'danger')
+            return redirect('/register') # reload the page
+        user = User(username=username, email=email, password=password)
+        add_to_db(user)
+    return render_template('register.html')
 
 @app.route('/file/upload', methods=['GET', 'POST'])
 def file_upload():
-    #code
+    if request.method == 'POST':
+        file = request.files['file']
+        name = secure_filename(file.filename)
+        path = upload_file(file, name)
+        file = File(path=path, user_id=1)
+        add_to_db(file)
+        flash("File uploaded successfully", 'success')
     return render_template('upload.html')
+
+@app.route('/file/list', methods=['GET','POST'])
+def view_files():
+   db = open_db()
+   files =  db.query(File).all()
+   return render_template('display_list.html', files=files)
+
+@app.route('/file/<int:id>/view/')
+def file_view(id):
+    return render_template('view_file.html')
 
 @app.route('/about')
 def about():
