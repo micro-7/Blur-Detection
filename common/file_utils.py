8243d30b4b2fc5e1dc52eafefe2d532bb2ef100a
mihir-tandon <mihirtandon25@gmail.com>
2024-04-14 11:55:09 +0530

    changes

--- common/file_utils.py
+++ common/file_utils.py
@@ -18,7 +18,7 @@ def is_file_allowed(file_path):
     return ext in allowed_extensions
 
 def upload_file(file, name):
-    upload_path = os.path.join('/static','uploads')
+    upload_path = os.path.join('static','uploads')
     if not os.path.exists(upload_path):
         os.makedirs(upload_path)
     # uncomment the below line to restrict file types
